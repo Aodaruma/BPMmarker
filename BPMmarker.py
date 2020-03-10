96ac65d378fff8b9265222cc17b18d68c8fa8183
Aodaruma <skyblue.1007.sakkyoku@gmail.com>
2020-03-10 14:38:21 +0900

    created

---
+++ BPMmarker.py
@@ -0,0 +1,89 @@
+import bpy
+
+bl_info = {
+    "name": "animation: BPM marker",
+    "author": "Aodaruma",
+    "version": (1, 0),
+    "blender": (2, 80, 0),
+    "location": "",
+    "description": "automatically marking beats with BPM",
+    "warning": "",
+    "support": "TESTING",
+    "wiki_url": "",
+    "tracker_url": "",
+    "category": "Animation"
+}
+####################################
+
+VERSION = bpy.app.version
+
+class UI(bpy.types.Panel):
+    bl_label = "BPM marker"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "TOOLS" if bpy.app.version < (2,80,0) else "UI"
+    bl_category = "Tools"
+    def draw(self, context):
+        self.layout.operator("my.mark", text="Mark")
+
+# ------------------------------------------------ #
+
+class MarkingButton(bpy.types.Operator):
+    bl_idname = "my.mark"
+    bl_label = "tetete"
+  
+#   def execute(self, context):
+#     print("pushed")
+#     return {'FINISHED'}
+    BPM = bpy.props.IntProperty(name="BPM",default=120)
+    beat = bpy.props.IntProperty(name="Beat",default=4)
+    isClearPreMark = bpy.props.BoolProperty(name="clear previous mark",default=True)
+    
+    def execute(self, context):
+        scene = bpy.context.scene
+
+        bpm = self.BPM
+        beat = self.beat
+        fps = scene.render.fps
+
+        if self.isClearPreMark:
+            scene.timeline_markers.clear()
+
+        fpb = 60 * fps / bpm
+        print("frames per beat", fpb)
+        frame = scene.frame_start
+        while frame < scene.frame_end:
+            counter = round(frame / fpb % beat) + 1
+            if VERSION < (2,80,0):
+                scene.timeline_markers.new("{m}{c}{m}".format(c=counter, m="|" if counter == 1 else ""), frame)
+            else:
+                scene.timeline_markers.new("{m}{c}{m}".format(c=counter, m="|" if counter == 1 else ""), frame=frame)
+            frame += fpb
+
+        return{'FINISHED'}
+
+    def invoke(self, context, event):
+        return context.window_manager.invoke_props_dialog(self)
+
+    
+classes = (
+    UI,
+    MarkingButton,
+)
+
+for cls in classes:
+  bpy.utils.register_class(cls)
+
+####################################
+
+def register():
+    print(bl_info["name"]+" ON")
+
+def unregister():
+    print(bl_info["name"]+" OFF")
+
+if __name__ == "__main__":
+    register()
+    
+####################################
+    
+# print("done!")
--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# BPMmark_maker
- the addon adding a marker to blender's timeline
+# BPMmarker
+ the addon adding a marker to Blender's timeline.
+
+## how to install & use
+
+1. download `BPMmarker.py`
+2. install it into Blender with `installing addon from file`
+3. find it in the toolshelf of 3Dviewport
+4. click the button and enjoy!
+
+## description of dialog
+
+- **BPM**
+  the number of BPM
+- **Beat**
+  the number of beats
+- **clear previous mark**
+  whether clear markers that created previously
+
+---
+
+Developed by Aodaruma ([@Aodaruma_](https://twitter.com/Aodaruma_))
+
+Please tell me if you have any questions or bug issues.
+
