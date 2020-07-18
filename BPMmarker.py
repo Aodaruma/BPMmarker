d72a66dece350e65a9515bce9f2a3c3612e3bb26
Aodaruma <skyblue.1007.sakkyoku@gmail.com>
2020-07-18 17:56:38 +0900

    Revert "Updated to version 1.1"
    This reverts commit 7773556b82a6afb461a4d7d5b6bc23fc52326582.

--- BPMmarker.py
+++ BPMmarker.py
@@ -3,8 +3,8 @@ import bpy
 bl_info = {
     "name": "animation: BPM marker",
     "author": "Aodaruma",
-    "version": (1, 1),
-    "blender": (2, 82, 0),
+    "version": (1, 0),
+    "blender": (2, 80, 0),
     "location": "",
     "description": "automatically marking beats with BPM",
     "warning": "",
@@ -17,38 +17,32 @@ bl_info = {
 
 VERSION = bpy.app.version
 
-
 class UI(bpy.types.Panel):
     bl_label = "BPM marker"
     bl_space_type = "VIEW_3D"
-    bl_region_type = "TOOLS" if bpy.app.version < (2, 80, 0) else "UI"
+    bl_region_type = "TOOLS" if bpy.app.version < (2,80,0) else "UI"
     bl_category = "Tools"
-
     def draw(self, context):
         self.layout.operator("aodaruma.bpmmarker", text="Mark")
 
 # ------------------------------------------------ #
 
-
 class MarkingButton(bpy.types.Operator):
-    bl_idname = "aodaruma.bpmmarker.mark"
+    bl_idname = "aodaruma.bpmmarker"
     bl_label = "BPM marker"
-
+  
 #   def execute(self, context):
 #     print("pushed")
 #     return {'FINISHED'}
-    BPM = bpy.props.IntProperty(name="BPM", default=120, min=1)
-    beat = bpy.props.IntProperty(name="Beats", default=4, min=1, max=256)
-    start = bpy.props.IntProperty(name="BeginFrame", default=0)
-    isClearPreMark = bpy.props.BoolProperty(
-        name="clear previous marks", default=True)
-
+    BPM = bpy.props.IntProperty(name="BPM",default=120)
+    beat = bpy.props.IntProperty(name="Beats",default=4)
+    isClearPreMark = bpy.props.BoolProperty(name="clear previous marks",default=True)
+    
     def execute(self, context):
         scene = bpy.context.scene
 
         bpm = self.BPM
         beat = self.beat
-        start = self.start
         fps = scene.render.fps
 
         if self.isClearPreMark:
@@ -56,16 +50,13 @@ class MarkingButton(bpy.types.Operator):
 
         fpb = 60 * fps / bpm
         print("frames per beat", fpb)
-        # frame = scene.frame_start
-        frame = start
+        frame = scene.frame_start
         while frame < scene.frame_end:
             counter = round(frame / fpb % beat) + 1
-            if VERSION < (2, 80, 0):
-                scene.timeline_markers.new("{m}{c}{m}".format(
-                    c=counter, m="|" if counter == 1 else ""), frame)
+            if VERSION < (2,80,0):
+                scene.timeline_markers.new("{m}{c}{m}".format(c=counter, m="|" if counter == 1 else ""), frame)
             else:
-                scene.timeline_markers.new("{m}{c}{m}".format(
-                    c=counter, m="|" if counter == 1 else ""), frame=frame)
+                scene.timeline_markers.new("{m}{c}{m}".format(c=counter, m="|" if counter == 1 else ""), frame=frame)
             frame += fpb
 
         return{'FINISHED'}
@@ -73,29 +64,26 @@ class MarkingButton(bpy.types.Operator):
     def invoke(self, context, event):
         return context.window_manager.invoke_props_dialog(self)
 
-
+    
 classes = (
     UI,
-    MarkingButton
+    MarkingButton,
 )
 
 for cls in classes:
-    bpy.utils.register_class(cls)
+  bpy.utils.register_class(cls)
 
 ####################################
-
 
 def register():
     print(bl_info["name"]+" registered.")
 
-
 def unregister():
     print(bl_info["name"]+" unregistered.")
 
-
 if __name__ == "__main__":
     register()
-
+    
 ####################################
-
+    
 # print("done!")
